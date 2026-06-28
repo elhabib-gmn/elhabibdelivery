b164d91b52c16b964b7389f56f33d275441d99d7
elhabib-gmn <gouahmaneelhabib@gmail.com>
2026-06-28 14:28:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Elhabib Delivery
 
+console.log("Elhabib Delivery جاهز ✅");
+
+// تأثير بسيط عند الضغط على الأزرار
+document.querySelectorAll(".btn, .btn2").forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("Button clicked");
+    });
+});
